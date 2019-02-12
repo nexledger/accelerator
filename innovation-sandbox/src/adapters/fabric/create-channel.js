97ebe531435e0c6fc4ba297e35858c7b827516f8
nexledger <nexledger.h@samsung.com>
2019-02-12 10:25:08 +0900

    init

---
+++ innovation-sandbox/src/adapters/fabric/create-channel.js
@@ -0,0 +1,141 @@
+/**
+ * Modifications Copyright 2017 HUAWEI
+ * Copyright 2016 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+'use strict';
+
+if (global && global.hfc) {
+    global.hfc.config = undefined;
+}
+
+require('nconf').reset();
+const utils = require('fabric-client/lib/utils.js');
+const Client = require('fabric-client');
+const fs = require('fs');
+
+const testUtil = require('./util.js');
+const commUtils = require('../../comm/util');
+const commLogger = commUtils.getLogger('create-channel.js');
+
+/**
+ * Create the channels located in the given configuration file.
+ * @param {string} config_path The path to the Fabric network configuration file.
+ * @async
+ */
+async function run(config_path) {
+    const fabric = commUtils.parseYaml(config_path).fabric;
+    const channels = fabric.channel;
+    if(!channels || channels.length === 0) {
+        throw new Error('No channel information found');
+    }
+
+    let ORGS = fabric.network;
+    let caRootsPath = ORGS.orderer.tls_cacerts;
+    let data = fs.readFileSync(commUtils.resolvePath(caRootsPath));
+    let caroots = Buffer.from(data).toString();
+    utils.setConfigSetting('key-value-store', 'fabric-client/lib/impl/FileKeyValueStore.js');
+
+    try {
+        for (let channel of channels) {
+            if(channel.deployed) {
+                continue;
+            }
+
+            commLogger.info(`Creating ${channel.name}...`);
+
+            // Acting as a client in first org when creating the channel
+            let client = new Client();
+            let org = channel.organizations[0];
+            let orderer = client.newOrderer(
+                ORGS.orderer.url,
+                {
+                    'pem': caroots,
+                    'ssl-target-name-override': ORGS.orderer['server-hostname']
+                }
+            );
+
+            let config = null;
+            let signatures = [];
+
+            let store = await Client.newDefaultKeyValueStore({path: testUtil.storePathForOrg(org)});
+            client.setStateStore(store);
+            let cryptoSuite = Client.newCryptoSuite();
+            cryptoSuite.setCryptoKeyStore(Client.newCryptoKeyStore({path: testUtil.storePathForOrg(org)}));
+            client.setCryptoSuite(cryptoSuite);
+
+            await testUtil.getOrderAdminSubmitter(client);
+
+            // use the config update created by the configtx tool
+            let envelope_bytes = fs.readFileSync(commUtils.resolvePath(channel.config));
+            config = client.extractChannelConfig(envelope_bytes);
+
+            // TODO: read from channel config instead of binary tx file
+
+            // sign the config for each org
+            for (let organization of channel.organizations) {
+                client._userContext = null;
+                await testUtil.getSubmitter(client, true, organization);
+                // sign the config
+                let signature = client.signChannelConfig(config);
+                // TODO: signature counting against policies on the orderer
+                // at the moment is being investigated, but it requires this
+                // weird double-signature from each org admin
+                signatures.push(signature);
+                signatures.push(signature);
+            }
+
+            client._userContext = null;
+            await testUtil.getOrderAdminSubmitter(client);
+
+            // sign the config
+            let signature = client.signChannelConfig(config);
+            // collect signature from orderer admin
+            // TODO: signature counting against policies on the orderer
+            // at the moment is being investigated, but it requires this
+            // weird double-signature from each org admin
+            signatures.push(signature);
+            signatures.push(signature);
+            // build up the create request
+            let tx_id = client.newTransactionID();
+            let request = {
+                config: config,
+                signatures : signatures,
+                name : channel.name,
+                orderer : orderer,
+                txId  : tx_id
+            };
+
+            // send create request to orderer
+            let result = await client.createChannel(request);
+            if(result.status && result.status === 'SUCCESS') {
+                commLogger.info(`Created ${channel.name} successfully`);
+            }
+            else {
+                throw new Error(`Create status for ${channel.name} is ${result.status}`);
+            }
+        }
+
+        commLogger.info('Sleeping 5s...');
+        await commUtils.sleep(5000);
+    } catch (err) {
+        commLogger.error(`Failed to create channels: ${(err.stack ? err.stack : err)}`);
+        throw err;
+    }
+}
+
+module.exports.run = run;
+
+
---
+++ innovation-sandbox/src/adapters/fabric/fabric.js
@@ -0,0 +1,171 @@
+/**
+ * Copyright 2017 HUAWEI. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * @file, definition of the Fabric class, which implements the caliper's NBI for hyperledger fabric
+ */
+
+'use strict';
+
+const util = require('./util.js');
+const e2eUtils = require('./e2eUtils.js');
+const impl_create = require('./create-channel.js');
+const impl_join = require('./join-channel.js');
+const impl_install = require('./install-chaincode.js');
+const impl_instantiate = require('./instantiate-chaincode.js');
+const BlockchainInterface = require('../../comm/blockchain-interface.js');
+const commUtils = require('../../comm/util');
+const commLogger = commUtils.getLogger('fabric.js');
+const TxStatus = require('../../comm/transaction');
+
+/**
+ * Implements {BlockchainInterface} for a Fabric backend.
+ */
+class Fabric extends BlockchainInterface{
+    /**
+     * Create a new instance of the {Fabric} class.
+     * @param {string} config_path The path of the Fabric network configuration file.
+     */
+    constructor(config_path) {
+        super(config_path);
+    }
+
+    /**
+     * Initialize the {Fabric} object.
+     * @async
+     */
+    async init() {
+        util.init(this.configPath);
+        e2eUtils.init(this.configPath);
+        try {
+            await impl_create.run(this.configPath);
+            await impl_join.run(this.configPath);
+        } catch (err) {
+            commLogger.error(`Fabric initialization failed: ${(err.stack ? err.stack : err)}`);
+            throw err;
+        }
+    }
+
+    /**
+     * Deploy the chaincode specified in the network configuration file to all peers.
+     * @async
+     */
+    async installSmartContract() {
+        // todo: now all chaincodes are installed and instantiated in all peers, should extend this later
+        try {
+            await impl_install.run(this.configPath);
+            await impl_instantiate.run(this.configPath);
+        } catch (err) {
+            commLogger.error(`Fabric chaincode install failed: ${(err.stack ? err.stack : err)}`);
+            throw err;
+        }
+    }
+
+    /**
+     * Return the Fabric context associated with the given callback module name.
+     * @param {string} name The name of the callback module as defined in the configuration files.
+     * @param {object} args unused.
+     * @param {Integer} clientIdx The client index.
+     * @param {Object} txFile the file information for reading or writing.
+     * @return {object} The assembled Fabric context.
+     * @async
+     */
+    async getContext(name, args, clientIdx, txFile) {
+        util.init(this.configPath);
+        e2eUtils.init(this.configPath);
+        this.txFile = txFile;
+        if(this.txFile){
+            this.txFile.name = name;
+            commLogger.debug('getContext) name: ' + name +  ' clientIndex: ' + clientIdx + ' txFile: ' + JSON.stringify(this.txFile));
+            if(this.txFile.readWrite === 'read') {
+                if(this.txFile.roundCurrent === 0){
+                    await e2eUtils.readFromFile(this.txFile.name);
+                }
+            }
+        }
+        let config  = require(this.configPath);
+        let context = config.fabric.context;
+        let channel;
+        if(typeof context === 'undefined') {
+            channel = util.getDefaultChannel();
+        }
+        else{
+            channel = util.getChannel(context[name]);
+        }
+
+        if(!channel) {
+            throw new Error('Could not find context information in the config file');
+        }
+
+        return await e2eUtils.getcontext(channel, clientIdx, txFile);
+    }
+
+    /**
+     * Release the given Fabric context.
+     * @param {object} context The Fabric context to release.
+     * @async
+     */
+    async releaseContext(context) {
+        if(this.txFile && this.txFile.readWrite === 'write') {
+            if(this.txFile.roundCurrent === (this.txFile.roundLength - 1)){
+                await e2eUtils.writeToFile(this.txFile.name);
+            }
+        }
+        await e2eUtils.releasecontext(context);
+        await commUtils.sleep(1000);
+    }
+
+    /**
+     * Invoke the given chaincode according to the specified options. Multiple transactions will be generated according to the length of args.
+     * @param {object} context The Fabric context returned by {getContext}.
+     * @param {string} contractID The name of the chaincode.
+     * @param {string} contractVer The version of the chaincode.
+     * @param {Array} args Array of JSON formatted arguments for transaction(s). Each element contains arguments (including the function name) passing to the chaincode. JSON attribute named transaction_type is used by default to specify the function name. If the attribute does not exist, the first attribute will be used as the function name.
+     * @param {number} timeout The timeout to set for the execution in seconds.
+     * @return {Promise<object>} The promise for the result of the execution.
+     */
+    async invokeSmartContract(context, contractID, contractVer, args, timeout) {
+        let promises = [];
+        args.forEach((item, index)=>{
+            try {
+                let simpleArgs = [];
+                let func;
+                for(let key in item) {
+                    if(key === 'transaction_type') {
+                        func = item[key].toString();
+                    }
+                    else {
+                        simpleArgs.push(item[key].toString());
+                    }
+                }
+                if(func) {
+                    simpleArgs.splice(0, 0, func);
+                }
+                promises.push(e2eUtils.invokebycontext(context, contractID, contractVer, simpleArgs, timeout));
+            }
+            catch(err) {
+                commLogger.error(err);
+                let badResult = new TxStatus('artifact');
+                badResult.SetStatusFail();
+                promises.push(Promise.resolve(badResult));
+            }
+        });
+        return await Promise.all(promises);
+    }
+
+    /**
+     * Query the given chaincode according to the specified options.
+     * @param {object} context The Fabric context returned by {getContext}.
+     * @param {string} contractID The name of the chaincode.
+     * @param {string} contractVer The version of the chaincode.
+     * @param {string} key The argument to pass to the chaincode query.
+     * @param {string} [fcn=query] The chaincode query function name.
+     * @return {Promise<object>} The promise for the result of the execution.
+     */
+    async queryState(context, contractID, contractVer, key, fcn = 'query') {
+        // TODO: change string key to general object
+        return await e2eUtils.querybycontext(context, contractID, contractVer, key.toString(), fcn);
+    }
+}
+module.exports = Fabric;
