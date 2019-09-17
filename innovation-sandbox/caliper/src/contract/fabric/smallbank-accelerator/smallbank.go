8b6ffbfb288ae03f91d7864994e61614317cce2c
SangoneLee <sangone.lee@samsung.com>
2019-09-17 09:01:15 +0900

    Revert last commit

--- innovation-sandbox/caliper/src/contract/fabric/smallbank-accelerator/smallbank.go
+++ innovation-sandbox/caliper/src/contract/fabric/smallbank-accelerator/smallbank.go
@@ -1,29 +1,20 @@
-/*
- *    Copyright 2019 Samsung SDS
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+/**
+* Copyright 2017 HUAWEI. All Rights Reserved.
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
 
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"crypto/sha512"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
-	"strconv"
 	"strings"
-
+	
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
@@ -37,6 +28,7 @@ const ERROR_PUT_STATE = "Failed to put state"
 var namespace = hexdigest("smallbank")[:6]
 
 type SmallbankChaincode struct {
+
 }
 
 func (t *SmallbankChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
@@ -49,38 +41,37 @@ func (t *SmallbankChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Respons
 
 	switch function {
 	case "create_account":
-		return Invoke(stub, t.CreateAccount)
+		return invoke(stub, t.CreateAccount)
 	case "transact_savings":
-		return Invoke(stub, t.TransactSavings)
+		return invoke(stub, t.TransactSavings)
 	case "deposit_checking":
-		return Invoke(stub, t.DepositChecking)
+		return invoke(stub, t.DepositChecking)
 	case "send_payment":
-		return Invoke(stub, t.SendPayment)
+		return invoke(stub, t.SendPayment)
 	case "write_check":
-		return Invoke(stub, t.WriteCheck)
+		return invoke(stub, t.WriteCheck)
 	case "amalgamate":
-		return Invoke(stub, t.Amalgamate)
+		return invoke(stub, t.Amalgamate)
 	case "query":
-		return Invoke(stub, t.Query)
+		return invoke(stub, t.Query)
 	default:
-		return errormsg(ERROR_UNKNOWN_FUNC + ": " + function)
+		return errormsg(ERROR_UNKNOWN_FUNC + ": " +function)
 	}
 }
 
 type Account struct {
-	CustomId        string
-	CustomName      string
-	SavingsBalance  int
+	CustomId   string
+	CustomName string
+	SavingsBalance int
 	CheckingBalance int
 }
-
 func (t *SmallbankChaincode) CreateAccount(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 4 { //should be [customer_id, customer_name, initial_checking_balance, initial_savings_balance]
+	if len(args) != 4 {	//should be [customer_id, customer_name, initial_checking_balance, initial_savings_balance]
 		return errormsg(ERROR_WRONG_ARGS + " create_account")
 	}
 
 	key := accountKey(args[0])
-	data, err := stub.GetState(key)
+	data,err := stub.GetState(key)
 	if data != nil {
 		return errormsg("Can not create duplicated account")
 	}
@@ -94,11 +85,11 @@ func (t *SmallbankChaincode) CreateAccount(stub shim.ChaincodeStubInterface, arg
 		return errormsg(ERROR_WRONG_ARGS + " create_account, saving balance should be integer")
 	}
 
-	account := &Account{
-		CustomId:        args[0],
-		CustomName:      args[1],
-		SavingsBalance:  saving,
-		CheckingBalance: checking}
+	account := &Account {
+		CustomId: args[0],
+		CustomName: args[1],
+		SavingsBalance: saving,
+		CheckingBalance: checking }
 	err = saveAccount(stub, account)
 	if err != nil {
 		return systemerror(err.Error())
@@ -108,12 +99,12 @@ func (t *SmallbankChaincode) CreateAccount(stub shim.ChaincodeStubInterface, arg
 }
 
 func (t *SmallbankChaincode) DepositChecking(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 { // should be [amount,customer_id]
+	if len(args) != 2 {	// should be [amount,customer_id]
 		return errormsg(ERROR_WRONG_ARGS + " deposit_checking")
 	}
 	account, err := loadAccount(stub, args[1])
 	if err != nil {
-		return errormsg(ERR_NOT_FOUND)
+		return errormsg(ERR_NOT_FOUND);
 	}
 	amount, _ := strconv.Atoi(args[0])
 	account.CheckingBalance += amount
@@ -126,12 +117,12 @@ func (t *SmallbankChaincode) DepositChecking(stub shim.ChaincodeStubInterface, a
 }
 
 func (t *SmallbankChaincode) WriteCheck(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 { // should be [amount,customer_id]
+	if len(args) != 2 {	// should be [amount,customer_id]
 		return errormsg(ERROR_WRONG_ARGS + " write_check")
 	}
 	account, err := loadAccount(stub, args[1])
 	if err != nil {
-		return errormsg(ERR_NOT_FOUND)
+		return errormsg(ERR_NOT_FOUND);
 	}
 	amount, _ := strconv.Atoi(args[0])
 	account.CheckingBalance -= amount
@@ -144,15 +135,15 @@ func (t *SmallbankChaincode) WriteCheck(stub shim.ChaincodeStubInterface, args [
 }
 
 func (t *SmallbankChaincode) TransactSavings(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 { // should be [amount,customer_id]
+	if len(args) != 2 {	// should be [amount,customer_id]
 		return errormsg(ERROR_WRONG_ARGS + " transaction_savings")
 	}
 	account, err := loadAccount(stub, args[1])
 	if err != nil {
-		return errormsg(ERR_NOT_FOUND)
+		return errormsg(ERR_NOT_FOUND);
 	}
 	amount, _ := strconv.Atoi(args[0])
-	// since the contract is only used for perfomance testing, we ignore this check
+	// since the contract is only used for perfomance testing, we ignore this check 
 	//if amount < 0 && account.SavingsBalance < (-amount) {
 	//	return errormsg("Insufficient funds in source savings account")
 	//}
@@ -166,7 +157,7 @@ func (t *SmallbankChaincode) TransactSavings(stub shim.ChaincodeStubInterface, a
 }
 
 func (t *SmallbankChaincode) SendPayment(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 3 { // should be [amount,dest_customer_id,source_customer_id]
+	if len(args) != 3 {	// should be [amount,dest_customer_id,source_customer_id]
 		return errormsg(ERROR_WRONG_ARGS + " send_payment")
 	}
 	destAccount, err1 := loadAccount(stub, args[1])
@@ -187,12 +178,12 @@ func (t *SmallbankChaincode) SendPayment(stub shim.ChaincodeStubInterface, args 
 	if err1 != nil || err2 != nil {
 		return errormsg(ERROR_PUT_STATE)
 	}
-
+	
 	return shim.Success(nil)
 }
 
 func (t *SmallbankChaincode) Amalgamate(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 { // should be [dest_customer_id,source_customer_id]
+	if len(args) != 2 {	// should be [dest_customer_id,source_customer_id]
 		return errormsg(ERROR_WRONG_ARGS + " amalgamate")
 	}
 	destAccount, err1 := loadAccount(stub, args[0])
@@ -208,13 +199,13 @@ func (t *SmallbankChaincode) Amalgamate(stub shim.ChaincodeStubInterface, args [
 	if err1 != nil || err2 != nil {
 		return errormsg(ERROR_PUT_STATE)
 	}
-
+	
 	return shim.Success(nil)
 }
 
 func (t *SmallbankChaincode) Query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	key := accountKey(args[0])
-	accountBytes, err := stub.GetState(key)
+	accountBytes,err := stub.GetState(key)
 	if err != nil {
 		return systemerror(err.Error())
 	}
@@ -222,7 +213,7 @@ func (t *SmallbankChaincode) Query(stub shim.ChaincodeStubInterface, args []stri
 	return shim.Success(accountBytes)
 }
 
-func main() {
+func  main()  {
 	err := shim.Start(new(SmallbankChaincode))
 	if err != nil {
 		fmt.Printf("Error starting chaincode: %v \n", err)
@@ -231,7 +222,7 @@ func main() {
 }
 
 func errormsg(msg string) pb.Response {
-	return shim.Error("{\"error\":" + msg + "}")
+	return shim.Error("{\"error\":"+msg+"}")
 }
 
 func systemerror(err string) pb.Response {
@@ -251,7 +242,7 @@ func accountKey(id string) string {
 
 func loadAccount(stub shim.ChaincodeStubInterface, id string) (*Account, error) {
 	key := accountKey(id)
-	accountBytes, err := stub.GetState(key)
+	accountBytes,err := stub.GetState(key)
 	if err != nil {
 		return nil, err
 	}
